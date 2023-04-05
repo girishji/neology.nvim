111a8ad4ea620c5130278dbe3c82a238f45f387f
Girish Palya <girishji@gmail.com>
2023-04-05 17:54:31 +0200

    added new variations of theme
    M  lua/lualine/themes/neology.lua
    M  lua/neology/colors.lua
    M  lua/neology/theme.lua

--- lua/lualine/themes/neology.lua
+++ lua/lualine/themes/neology.lua
@@ -3,29 +3,34 @@ local c = require("neology.colors").setup()
 local M = {}
 
 M.normal = {
-  a = { bg = c.bg_primary, fg = c.fg, gui = 'bold' },
+  a = { bg = c.bg_primary, fg = c.fg },
   b = { bg = c.bg, fg = c.fg_primary },
-  c = { bg = c.bg, fg = c.fg_overlay3 },
+  c = { bg = c.bg, fg = c.fg_overlay2 },
 }
 
 M.insert = {
-  a = { bg = c.bg_yellow, fg = c.fg, gui = 'bold' },
+  a = { bg = c.bg_gray, fg = c.fg },
+  b = { bg = c.bg, fg = c.fg_gray },
 }
 
 M.command = {
-  a = { bg = c.bg_purple, fg = c.fg, gui = 'bold' },
+  a = { bg = c.bg_brown, fg = c.fg },
+  b = { bg = c.bg, fg = c.fg_brown },
 }
 
 M.visual = {
-  a = { bg = c.bg_green, fg = c.fg, gui = 'bold' },
+  a = { bg = c.bg_purple, fg = c.fg },
+  b = { bg = c.bg, fg = c.fg_purple },
 }
 
 M.replace = {
-  a = { bg = c.bg_blue, fg = c.fg, gui = 'bold' },
+  a = { bg = c.bg_blue, fg = c.fg },
+  b = { bg = c.bg, fg = c.fg_blue },
 }
 
 M.terminal = {
-  a = { bg = c.bg_green, fg = c.fg, gui = 'bold' },
+  a = { bg = c.bg_blue, fg = c.fg },
+  b = { bg = c.bg, fg = c.fg_blue },
 }
 
 -- inactive when there are split windows
--- lua/neology/colors.lua
+++ lua/neology/colors.lua
@@ -2,6 +2,7 @@
 --   https://m2.material.io/design/color/the-color-system.html#tools-for-picking-colors
 --   https://m2.material.io/design/color/the-color-system.html#tools-for-picking-colors
 --   https://m2.material.io/inline-tools/color/
+--   https://compilezero.medium.com/dark-mode-ui-design-the-definitive-guide-part-1-color-53dcfaea5129
 
 local M = {}
 
@@ -9,29 +10,63 @@ local M = {}
 local palette = {
   basic_colors = {
     -- choose 200 saturation for text and >400 for background (statusline)
-    fg_red    = '#F48FB1',
+    fg_red    = '#EF9A9A',
     fg_yellow = '#FFF59D',
-    bg_yellow = '#827717',
+    bg_yellow = '#F57F17',
     fg_green  = '#80cbc4',
-    bg_green  = '#4CAF50',
-    bg_purple = '#9C27B0',
-    bg_blue   = '#03A9F4',
+    fg_orange = '#FFCC80',
+    fg_brown  = '#BCAAA4',
+    bg_brown  = '#4E342E',
+    fg_purple = '#CE93D8',
+    bg_purple = '#6A1B9A',
+    fg_blue   = '#80DEEA',
+    bg_blue   = '#006064',
+    fg_gray   = '#B0BEC5',
+    bg_gray   = '#455A64',
   },
-  default = {
+  tricolor = {
     bg = {
-      bg = '#0a1930',
-      bg_overlay0 = '#19243b', -- cursorline --172135
-      bg_overlay1 = '#1a2947',
-      bg_overlay2 = '#2a426a',
+      bg = '#1e1e2e',
+      bg_overlay0 = "#282939", --cursorline
+      bg_overlay1 = '#282939',
+      bg_overlay2 = '#35364c',
+      bg_overlay3 = '#4f5179',
+      -- Bluish background
+      -- bg = '#0a1930',
+      -- bg_overlay0 = '#19243b', -- cursorline --172135
+      -- bg_overlay1 = '#1a2947',
+      -- bg_overlay2 = '#2a426a',
+      -- bg_overlay3 = '#4c5773',
       bg_primary = '#007e65',
     },
     fg = {
-      fg = '#E0E0E0',
-      '#e7eef9',
-      fg_overlay1 = '#BDBDBD',
-      fg_overlay2 = '#BDBDBD',
-      fg_overlay3 = '#757575',
-      fg_overlay4 = '#757575',
+      -- Bluish tones
+      fg = '#ddd7d1',
+      fg_overlay1 = '#c9d2ef',
+      fg_overlay2 = '#9197ab',
+      fg_overlay3 = '#8c92a5',
+      -- fg_overlay4 = '#646c86',
+      fg_overlay4 = '#484f63',
+      fg_overlay5 = '#484f63',
+      fg_primary = '#7AC5B5',
+    },
+  },
+  --
+  pure_gray = {
+    bg = {
+      bg = '#1e1e2e',
+      bg_overlay0 = "#282939", --cursorline
+      bg_overlay1 = '#282939',
+      bg_overlay2 = '#35364c',
+      bg_overlay3 = '#4f5179',
+      bg_primary = '#007e65',
+    },
+    fg = {
+      fg = '#e6ded6',
+      fg_overlay1 = '#C5C5C5',
+      fg_overlay2 = '#909090',
+      fg_overlay3 = '#909090',
+      fg_overlay4 = '#484f63',
       fg_overlay5 = '#424242',
       fg_primary = '#7AC5B5',
     },
@@ -43,16 +78,27 @@ local palette = {
       bg_overlay0 = "#282939", --cursorline
       bg_overlay1 = '#263238',
       bg_overlay2 = '#37474F',
+      bg_overlay3 = '#455A64',
       bg_primary = '#007e65',
     },
     fg = {
-      fg = '#e7eef9',
-      fg_overlay1 = '#a8b8ca',
-      fg_overlay2 = '#a8b8ca',
-      fg_overlay3 = '#70879f',
-      fg_overlay4 = '#70879f',
-      fg_overlay5 = '#4b647c',
+      fg = '#ddd7d1',
+      fg_overlay1 = '#aab2cc',
+      fg_overlay2 = '#aab2cc',
+      -- fg_overlay1 = '#9da3ba',
+      -- fg_overlay2 = '#9da3ba',
+      fg_overlay3 = '#8c92a5',
+      -- fg_overlay4 = '#646c86',
+      fg_overlay4 = '#484f63',
+      fg_overlay5 = '#484f63',
       fg_primary = '#7AC5B5',
+      -- fg = '#e7eef9',
+      -- fg_overlay1 = '#a8b8ca',
+      -- fg_overlay2 = '#a8b8ca',
+      -- fg_overlay3 = '#70879f',
+      -- fg_overlay4 = '#70879f',
+      -- fg_overlay5 = '#4b647c',
+      -- fg_primary = '#7AC5B5',
     },
   },
   --
@@ -62,44 +108,28 @@ local palette = {
       bg_overlay0 = "#272d35", --cursorline
       bg_overlay1 = '#1f2933',
       bg_overlay2 = '#323f4b',
-      bg_primary = '#e9c9a8',
+      bg_overlay3 = '#3e4c59',
+      -- bg_primary = '#e9c9a8',
+      bg_primary = '#007e65',
     },
     fg = {
-      fg = '#f5f7fa',
-      fg_overlay1 = '#cbd2d9',
-      fg_overlay2 = '#cbd2d9',
+      fg = '#e4e7eb',
+      fg_overlay1 = '#9aa5b1',
+      fg_overlay2 = '#9aa5b1',
       fg_overlay3 = '#7b8794',
-      fg_overlay4 = '#7b8794',
+      fg_overlay4 = '#52606d',
       fg_overlay5 = '#52606d',
       fg_primary = '#7AC5B5',
     },
   },
-  -- saturation = {
-  --   '#f5f7fa',
-  --   '#e4e7eb',
-  --   '#cbd2d9',
-  --   '#9aa5b1',
-  --   '#7b8794',
-  --   '#616e7c',
-  --   '#52606d',
-  --   '#3e4c59',
-  --   '#323f4b',
-  --   '#1f2933',
-  -- },
-  -- bg = '#1c2025', -- bg color : overlay #121212 with font color at 8% opacity
-  -- cursorline = "#272d35",
-  -- -- bg = '#1e1e2e',
-  -- -- cursorline = "#282939",
-  -- statusline1 = "#444658",
-  -- statusline2 = "#161622",
-  -- secondary = '#62d0bf',
-  -- },
+  --
   warm_gray = {
     bg = {
       bg = '#241f1d',
       bg_overlay0 = "#2D2920",
       bg_overlay1 = '#27241d',
       bg_overlay2 = '#423d33',
+      bg_overlay3 = '#504a40',
       bg_primary = '#bc456b',
     },
     fg = {
@@ -111,24 +141,6 @@ local palette = {
       fg_overlay5 = '#625d52',
       fg_primary = '#e8a0b9',
     },
-    -- saturation = {
-    --   '#faf9f7',
-    --   '#e8e6e1',
-    --   '#d3cec4',
-    --   '#b8b2a7',
-    --   '#a39e93',
-    --   '#857f72',
-    --   '#625d52',
-    --   '#504a40',
-    --   '#423d33',
-    --   '#27241d',
-    -- },
-    -- bg = '#241f1d',
-    -- cursorline = "#2D2920",
-    -- statusline1 = "#423d33",
-    -- statusline2 = "#363229",
-    -- primary = '#e8a0b9',
-    -- secondary = '#62cfbe',
   },
 }
 
@@ -162,77 +174,11 @@ local get_palette = function(hue)
 end
 
 
--- -- Palette
--- local color = {
---   neutral = {
---     white         = "#F5EEE6",
---     black         = "#222222",
---     subtle_white  = "#ECDECE",
---     lightest_gray = "#CCCCCC",
---     lighter_gray  = "#a9a9a9",
---     light_gray    = "#999999",
---     medium_gray   = "#767676",
---     dark_gray     = "#545454",
---     darker_gray   = "#424242",
---     darkest_gray  = "#303030",
---     statusline1   = "#444658",
---     statusline2   = "#161622",
---     yellow        = "#FED442",
---     pink          = "#ce626c",
---     green         = "#22863A",
---     red           = "#e8303d",
---     orange        = "#E36209",
---     blue          = "#005CC5",
---   },
---   bluish = {
---     white         = "#e6ded6",
---     black         = "#1e1e2e",
---     -- subtle_white  = "#c9d2ef",
---     subtle_white  = "#ddd7d1",
---     -- subtle_white  = "#ddd7d1",
---     lightest_gray = "#CCCCCC",
---     lighter_gray  = "#c9d2ef", -- bluish
---     light_gray    = "#9197ab",
---     medium_gray   = "#8c92a5",
---     dark_gray     = "#646c86",
---     darker_gray   = "#484f63",
---     darkest_gray  = "#282939",
---     statusline1   = "#444658",
---     statusline2   = "#161622",
---     yellow        = "#f9e2af",
---     pink          = "#ce626c",
---     green         = "#93e2d5",
---     red           = "#f38ba8",
---     orange        = "#f3ae83",
---     blue          = "#85aff5",
---     purple        = "#c6a2f1",
---   },
--- }
-
--- local get_theme_colors = function(hue)
---   local c = hue == 'neutral' and color.neutral or color.bluish
---   local theme_colors = {
---     norm  = c.subtle_white,
---     -- fg    = c.white,
---     fg    = c.subtle_white,
---     bg    = c.black,
---     mute1 = c.lightest_gray,
---     mute2 = c.lighter_gray,
---     mute3 = c.medium_gray,
---     mute4 = c.dark_gray,
---     mute5 = c.darker_gray,
---     mute6 = c.darkest_gray,
---   }
---   return theme_colors
--- end
-
-
 function M.setup()
-  -- return get_palette('default')
-  local theme = 'default'
-  -- local theme = 'blue_gray'
-  -- local theme = 'warm_gray'
+  local theme = 'blue_gray'
   -- local theme = 'cool_gray'
+  -- local theme = 'tricolor'
+  -- local theme = 'warm_gray'
   local colors = vim.tbl_extend('force', {}, palette.basic_colors)
   colors = vim.tbl_extend('force', colors, palette[theme].fg)
   colors = vim.tbl_extend('force', colors, palette[theme].bg)
